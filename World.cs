6cdfc8cd2bb217b86ab8108b09f4d947fbc00875
Katrine Jessing <katrinejessingg@gmail.com>
2025-10-21 12:16:29 +0200

    added more to world map

--- World.cs
+++ World.cs
@@ -6,13 +6,26 @@ class World {
   
   public World () {
     Space entry    = new Space("Landbrugsskolen");
+// hovedrum mose:
     Space mose = new Space("Mosen");
+// underrum mose:
+    Space mosekant = new Space("Mosekant");
+    Space tørvemose = new Space ("Tørvemosen");
+    Space udtørretEng = new Space ("Udtørret eng");
+
+// hovedrum 2 (mark)
     Space mark     = new Space("Marken");
+// underrum
+
+//hovedrum 3 (vandløb)
     Space vandløb      = new Space("Vandløbet");
     
     entry.AddEdge("north", mose);
     entry.AddEdge("south", mark);
     entry.AddEdge("east", vandløb);
+    mose.AddEdge("mosekant", mosekant);
+    mosekant.AddEdge("tørvemose", tørvemose);
+    udtørretEng.AddEdge("engen", udtørretEng);
     
     
     this.entry = entry;
